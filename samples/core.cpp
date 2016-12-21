692a7e0ba01ac9af86e044a5ee507ed8d834690c
Alexandr Konakov <konakov@my.com>
2016-12-22 02:29:50 +0300

    Чуть приукрасил графику

--- samples/core.cpp
+++ samples/core.cpp
@@ -17,31 +17,32 @@ int main(int argc, char * argv[])
     str = "50*x";
     by_x.loadExpr(string(str));
 
-    str = "-abs(600*cos(x))";
-    //std::cin >> str;
+    str = "-300 * cos(x)";
 
     by_y.loadExpr(string(str));
 
     Mat image = Mat::zeros(WIDTH, WIDTH, CV_8UC3);
     double delta = M_PI/16.0;
 
+    line( image, Point(0, WIDTH/2), Point(WIDTH, WIDTH/2), Scalar( 0 , 0, 255 ),  2, 1);
+    line( image, Point(WIDTH/2, 0), Point(WIDTH/2, WIDTH), Scalar( 0 , 255, 0 ),  2, 1);
     try
     {
-    for(double a = 0.0; a < M_PI * 5.0; a += delta)
-    {
-        by_x.loadVariable("x", a);
-        by_y.loadVariable("x", a);
-        
-        Point p1((int)(by_x.compute()), (int)(by_y.compute()) + WIDTH);
+        for(double a = 0.0; a < M_PI * 5.0; a += delta)
+        {
+            by_x.loadVariable("x", a);
+            by_y.loadVariable("x", a);
+   
+            Point p1((int)(by_x.compute()), (int)(by_y.compute()) + WIDTH/2);
 
-        by_x.loadVariable("x", a + delta);
-        by_y.loadVariable("x", a + delta);
+            by_x.loadVariable("x", a + delta);
+            by_y.loadVariable("x", a + delta);
 
-        Point p2((int)(by_x.compute()), (int)(by_y.compute()) + WIDTH);
+            Point p2((int)(by_x.compute()), (int)(by_y.compute()) + WIDTH/2);
     
-        line( image, p1, p2, Scalar( 255 , 255, 255 ),  1, 8);
-        imshow(str,image);
-    }
+            line( image, p1, p2, Scalar( 255, 127, 0),  2, 16);
+            imshow(str,image);
+        }
     }
     catch(MathExpr::ExprException &e)
     {
